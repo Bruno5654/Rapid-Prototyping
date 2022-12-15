6bc93c95965d42b6323074e4da1d286c5185c63c
Bruno Reeve <114944836+Bruno5654@users.noreply.github.com>
2022-12-15 14:47:50 +0000

    In Lesson work

--- Assets/Custom Scripts/Vertical/EnemySpawner.cs
+++ Assets/Custom Scripts/Vertical/EnemySpawner.cs
@@ -7,31 +7,22 @@ public class EnemySpawner : MonoBehaviour
     public GameObject enemyToSpawn;
     public float spawnRate;
 
-    private float time = 0;
     private int spawned = 0;
     private int countToSpawn;
 
     void Start()
     {
-        countToSpawn = Random.Range(1, 2);
+        
     }
 
     void Update()
     {
-        time += Time.deltaTime;
-        if(time>spawnRate)
+        
+        
+        spawned++;
+        if (spawned > countToSpawn)
         {
-            GameObject enemy = Instantiate(enemyToSpawn, transform.position, transform.rotation);
-            enemy.GetComponent<RedPlagueMover>().maxXOffset = Random.Range(-8.0f, 8.0f);
-
-
-            time = 0;
-            spawned++;
-            if(spawned > countToSpawn)
-            {
-                Destroy(gameObject);
-            }
-            
+            Destroy(gameObject);
         }
     }
 }
--- Assets/Custom Scripts/Vertical/GameManager.cs
+++ Assets/Custom Scripts/Vertical/GameManager.cs
@@ -6,26 +6,61 @@ using TMPro;
 
 public class GameManager : MonoBehaviour
 {
-    private float time;
+    private float runningTime;
+    private float timeForSpawn;
+    GameObject enemy;
     public int score;
     
+
     public int enemyScore;
-    public GameObject enemySpawner;
+    public GameObject enemyToSpawn;
     public TMP_Text scoreText;
 
     // Start is called before the first frame update
-    void Start()
+    void SpawnEnemy()
     {
-        Instantiate(enemySpawner, transform.position, transform.rotation);
+        enemy = Instantiate(enemyToSpawn, transform.position, transform.rotation);
+        enemy.GetComponent<RedPlagueMover>().maxXOffset = Random.Range(-9.0f, 9.0f);
+        
     }
+   
 
     // Update is called once per frame
     void Update()
     {
-        time = Time.deltaTime;
-        score = (int)time + enemyScore;
+        runningTime += Time.deltaTime;
+        timeForSpawn += Time.deltaTime;
+        score = (int)runningTime + enemyScore;
         scoreText.text = score.ToString();
+
+        if(timeForSpawn >= 5)
+        {
+            timeForSpawn = 0;
+            int randNum = Random.Range(1, 3);
+            switch(randNum)
+            {
+                case 1:
+                    SpawnEnemy();
+                    break;
+                case 2:
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    break;
+                case 3:
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    break;
+                case 4:
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    SpawnEnemy();
+                    break;
+            }
+            
+        }
     }
 
-
+    
 }
