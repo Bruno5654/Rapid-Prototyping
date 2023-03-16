315732ae89a559eeeab3731cbade160066b852e7
Bruno Reeve <114944836+Bruno5654@users.noreply.github.com>
2023-03-16 14:36:08 +0000

    Week4

--- Assets/Scripts/BasicPlatformer/BasicCharacterController.cs
+++ Assets/Scripts/BasicPlatformer/BasicCharacterController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.InputSystem;
 
 //--------------------------------------------
 /*Basic Character Controller Includes:  
@@ -17,122 +18,71 @@ public class BasicCharacterController : MonoBehaviour
 
     public float speed = 5.0f;
     public float jumpForce = 1000.0f;
-    public float dashForce = 10.0f;
+   
 
-    private float horizInput;
-    private float vertInput;
-    private float tempJumpPower = 0.0f;
     private float jumpPower = 0.0f;
-    private bool canDash = true;
+    private float horizInput = 0.0f;
 
     public Transform groundedCheckStart;
     public Transform groundedCheckEnd;
     public bool grounded;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
     void Awake()
     {
         rb = GetComponent<Rigidbody2D>();
     }
 
+    public void Jump(InputAction.CallbackContext context)
+    {
+        Debug.Log("Jumping");
+
+        if(context.performed)
+        {
+            if (grounded == true)
+            {
+                jumped = true;
+                Debug.Log("Should jump");
+            }
+        }
+       
+
+        if (jumped == true)
+        {
+            rb.AddForce(new Vector2(0f, jumpForce));
+            Debug.Log("Jumping!");
+            jumped = false;
+        }
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        float inputVector = context.ReadValue<float>();
+        horizInput = inputVector;
+        animator.SetFloat("Speed", Mathf.Abs(horizInput));
+
+    }
+
     void Update()
     {
-        //Get Player input 
-        horizInput = Input.GetAxis("Horizontal");
-        vertInput = Input.GetAxis("Vertical"); 
+      
     }
-    
-
     void FixedUpdate()
     {
         //Linecast to our groundcheck gameobject if we hit a layer called "Level" then we're grounded
         grounded = Physics2D.Linecast(groundedCheckStart.position, groundedCheckEnd.position, 1 << LayerMask.NameToLayer("Level"));
         Debug.DrawLine(groundedCheckStart.position, groundedCheckEnd.position, Color.red);
 
-        //Calculate jump power.
-        if (vertInput > 0 && grounded == true)
-        {
-            tempJumpPower += 0.03f;
-            if(tempJumpPower > 1.1f)
-            {
-                tempJumpPower = 1.1f;
-            }
-        }
-
-        if(vertInput == 0 && tempJumpPower > 0)
-        {
-            jumpPower = tempJumpPower;
-            tempJumpPower = 0.0f;
-        }
-
         //Move Character
-        if(grounded == false)
+        if (grounded == false)
         {
             rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y);
         }
         else
         {
-            canDash = true;
             rb.velocity = new Vector2(horizInput * speed * Time.fixedDeltaTime, rb.velocity.y);
         }
-        
-        
-        //Dash
-        if(horizInput != 0 && canDash == true && grounded == false)
-        {
-            dashed = true;
-            Debug.Log("Should Dash.");
-        }
-        
-        if (dashed == true)
-        {
-            if(horizInput > 0)
-            {
-                dashed = true;
-                Debug.Log("Dashing");
-                rb.AddForce(new Vector2(dashForce, 0.0f));
-                dashed = false;
-            }
-            if (horizInput < 0)
-            {
-                dashed = true;
-                Debug.Log("Dashing");
-                rb.AddForce(new Vector2(-dashForce, 0.0f));
-                dashed = false;
-            }
-        }
-     
-        //Jump
-        if (jumpPower > 0 && grounded == true)
-        {
-            jumped = true;
-            Debug.Log("Should jump");
-        }
-
-        if (jumped == true)
-        {
-            rb.AddForce(new Vector2(0f, jumpForce * jumpPower));
-            Debug.Log("Jumping!");
-            jumpPower = 0.0f;
-            jumped = false;
-        }
-
-        // Detect if character sprite needs flipping
-        if (horizInput > 0 && !facingRight)
-        {
-            FlipSprite();
-        }
-        else if (horizInput < 0 && facingRight)
-        {
-            FlipSprite();
-        }
-    }
-
-    // Flip Character Sprite
-    void FlipSprite()
-    {
-        facingRight = !facingRight;
-        transform.Rotate(0, 180, 0);
     }
 }
